7b4451ac626d4077ee4163908db078ad02f65541
Kevin Otok <kevinotok8@gmail.com>
2026-07-28 15:54:06 +0100

    Update __init__.py

--- src/human_creator/__init__.py
+++ src/human_creator/__init__.py
@@ -1,1 +1,20 @@
 
+import bpy
+from .ui.main_panel import OBJECT_PT_human_creator_panel
+
+classes = (
+    OBJECT_PT_human_creator_panel,
+)
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    print("Human Creator Extension Registered Successfully.")
+
+def unregister():
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
+    print("Human Creator Extension Unregistered.")
+
+if __name__ == "__main__":
+    register()
